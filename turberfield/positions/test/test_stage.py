cf0eaa820c780cbf4a0da9408260642d6cbdaca7
tundish <tundish@thuswise.org>
2014-10-26 18:31:04 +0000

    First working test of stage directions.

--- turberfield/positions/test/test_stage.py
+++ turberfield/positions/test/test_stage.py
@@ -16,27 +16,42 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
 
+from collections import namedtuple
 from collections import OrderedDict
 from collections.abc import MutableMapping
 import unittest
 import uuid
 
+from altgraph.Graph import Graph
+ 
 from turberfield.common.schema import Actor
 
 class Stage(MutableMapping):
-    """
-    Vista
-    Pitch
-    Chamber
-    Reach
-    """
+
+    Contact = namedtuple("Contact", ["view", "throw", "hear", "reach"])
+
+    class Directions():
+
+        def __init__(self, stage, obj):
+            self.stage = stage
+            self.obj = obj
+
+        def reaches(self, *args, fact=True, mutual=False):
+            for i in args:
+                other = self.stage[i]
+                self.stage.placement.add_edge(self.obj, other.obj)
+                edge = self.stage.placement.edge_by_node(self.obj, other.obj)
+                self.stage.placement.update_edge_data(
+                    edge,
+                    Stage.Contact(True, True, True, True)
+                )
 
     @staticmethod
     def item():
         return
 
     def __init__(self):
-        self.placement = OrderedDict()
+        self.placement = Graph()
 
     def __len__(self):
         return len(self.placement)
@@ -48,15 +63,27 @@ class Stage(MutableMapping):
         return self.placement.__delitem__(key)
 
     def __getitem__(self, key):
-        return self.placement.setdefault(key, None)
+        if key not in self.placement:
+            self.placement.add_node(key, Stage.Directions(self, key))
+        return self.placement.describe_node(key)[1]
 
     def __setitem__(self, key, val):
         return self.placement.__setitem__(key, val)
 
 class APIPrototyping(unittest.TestCase):
 
-    def test_movement(self):
+    def test_stage_stores_directions(self):
         s = Stage()
-        s[Actor(uuid.uuid4().hex, "Alice")].reaches({
-            Actor(uuid.uuid4().hex, "Bob")
-        }, fact=True, mutual=True)
+        rv = s[Actor(uuid.uuid4().hex, "Alice")]
+        self.assertIsInstance(rv, Stage.Directions)
+
+    def test_movement(self):
+        alice = Actor(uuid.uuid4().hex, "Alice")
+        bob = Actor(uuid.uuid4().hex, "Bob")
+        stage = Stage()
+        stage[alice].reaches(
+            bob,
+            fact=True, mutual=True)
+
+        self.assertIn(alice, stage)
+        self.assertIn(bob, stage)
