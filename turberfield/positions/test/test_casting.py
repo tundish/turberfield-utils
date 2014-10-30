62c192789f0a1ef616978782f766cb133751daab
boss on Apple Mac <>
2014-10-30 17:08:43 +0000

    Quickly populating test data.

--- turberfield/positions/test/test_casting.py
+++ turberfield/positions/test/test_casting.py
@@ -16,8 +16,44 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
 
+import enum
 import unittest
 import uuid
 
+from turberfield.common.schema import NPC
+from turberfield.common.schema import Player
+
+@enum.unique
+class Commodity(enum.Enum):
+    COIN = "coin"
+    SCRP = "scrip"
+
+@enum.unique
+class TradePosition(enum.Enum):
+
+    buying = "buying"
+    selling = "selling"
+
+actors = {
+    NPC(uuid.uuid4().hex, "worker #01", 0, 0, 0, 0.1): {
+        Commodity.COIN: [TradePosition.buying],
+        Commodity.SCRP: [TradePosition.selling],
+    },
+    NPC(uuid.uuid4().hex, "worker #02", 0, 0, 0, 0.2): {},
+    NPC(uuid.uuid4().hex, "worker #03", 0, 0, 0, 0.3): {},
+    NPC(uuid.uuid4().hex, "worker #04", 0, 0, 0, 0.4): {},
+    NPC(uuid.uuid4().hex, "worker #05", 0, 0, 0, 0.5): {},
+    NPC(uuid.uuid4().hex, "worker #06", 0, 0, 0, 0.6): {},
+    NPC(uuid.uuid4().hex, "worker #07", 0, 0, 0, 0.7): {},
+    Player(uuid.uuid4().hex, "Alice", 0, 0, 0, "alice@wonderland.com"): {
+    },
+    Player(uuid.uuid4().hex, "Bob", 0, 0, 0, "bob.cratchet@mail.me"): {
+    },
+    Player(uuid.uuid4().hex, "Carl", 0, 0, 0, "carl@buttonmasher.net"): {
+    },
+}
+
 class PrototypeCasting(unittest.TestCase):
-    pass
+
+    def test_payoff_scenario(self):
+        self.fail(actors)
