9ad1c3c8b6235b6a7089c4bbeea84187f623231c
Tundish <tundish@thuswise.org>
2016-12-30 17:40:04 +0000

    Added enums for file options.

--- turberfield/utils/test/test_db.py
+++ turberfield/utils/test/test_db.py
@@ -16,23 +16,44 @@
 # You should have received a copy of the GNU General Public License
 # along with turberfield.  If not, see <http://www.gnu.org/licenses/>.
 
+import enum
 import glob
 import os.path
 import tempfile
 import unittest
 import uuid
 
+
 class Connection:
     """
     * Find target database files
-    * Select module
     * Load extensions
-    * Attach databases
+    * Attach databases (readonly?)
+
+    * Attach in-memory database
     * Execute pragmas
+    * Discover state tables
+    * Create state tables
     """
+    class CacheOptions(enum.Enum):
+        shared = "cache=shared"
+        private = "cache=private"
+
+    class ImmutableOptions(enum.Enum):
+        immutable = "immutable=1"
+        mutable = "immutable=0"
+
+    class ModeOptions(enum.Enum):
+        readonly = "mode=ro"
+        readwrite = "mode=rw"
+        readwritecreate = "mode=rwc"
+        memory = "mode=memory"
+
     @staticmethod
-    def options():
-        return {}
+    def options(paths=[]):
+        return {
+            "attach": {i: [Connection.ModeOptions.readonly] for i in paths}
+        }
 
 class NeedsTempDirectory:
 
@@ -66,9 +87,17 @@ class OptionTests(NeedsTempDirectory, unittest.TestCase):
         paths = glob.glob(os.path.join(self.drcty.name, "*.db"))
         self.assertEqual(1, len(paths)) 
         OptionTests.close_db_files((fd, path))
+        rv = Connection.options(paths=paths)
+        self.assertIsInstance(rv, dict)
+        self.assertEqual(1, len(rv["attach"]))
+        self.assertTrue(all(Connection.ModeOptions.readonly in i for i in rv["attach"].values()))
 
     def test_ten_db_on_file(self):
         items = list(OptionTests.make_db_files(self.drcty, 10))
         paths = glob.glob(os.path.join(self.drcty.name, "*.db"))
         self.assertEqual(10, len(paths)) 
         OptionTests.close_db_files(*items)
+        rv = Connection.options(paths=paths)
+        self.assertIsInstance(rv, dict)
+        self.assertEqual(10, len(rv["attach"]))
+        self.assertTrue(all(Connection.ModeOptions.readonly in i for i in rv["attach"].values()))
